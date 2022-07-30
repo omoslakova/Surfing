1871e2d97ce381197c182c8a6744e24ec21a18cb
omoslakova <o.moslakova@yandex.by>
2022-07-30 14:23:21 +0300

    buttons added

--- src/App.js
+++ src/App.js
@@ -6,16 +6,30 @@ import { Container, Toolbar, Typography, IconButton, Box } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(1)
+  },
+  title: {
+    flexGrow:1
+  }
+}))
+
 function App() {
+  const classes = useStyles();
+
   return (
     <AppBar position="fixed">
       <Container fixed>
         <Toolbar>
           <IconButton edge="start"
-          color="inherit" aria-laabel="menu">
+          color="inherit" aria-laabel="menu" className={classes.menuButton}>
             <MenuIcon />
           </IconButton>
-<Typography variant="h6">Surfing</Typography>
+<Typography variant="h6" className={classes.title}>Surfing</Typography>
 <Box mr={3}>
   <Button color="inherit" variant="outlined">Log In</Button>
 </Box>
